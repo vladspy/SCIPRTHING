5a46004b3d2036fca09b15715ce0d5e255d44cfa
Vlad <vlad.ungureanu0007@gmail.com>
2026-04-15 12:37:34 +0200

    commiting game, might work?

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -4,10 +4,53 @@
  */
 package Model;
 
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.ArrayList;
+
 /**
  *
  * @author vladu
  */
 public class Game {
+    private int n;
+    private int stickCount;
+    
+    private ArrayList<Stick> alSticks = new ArrayList<>();
+    
+    public Game(int n){
+        this.n=n;
+        
+        for (int i = 0; i < n; i++) {
+            if(i%2 == 0){
+                alSticks.add(new HorizontalStick());
+            }else if(i%2 ==1){
+                alSticks.add(new VerticalStick());
+            }
+            
+        }
+    }
+    
+    public int getStickCount(){
+        return stickCount;
+    }
+    
+    public void clicked(Point p){
+        for(Stick s : alSticks){
+            if(s.collides(p)){
+                alSticks.remove(s);
+                stickCount--;
+            }
+        }
+    }
+    
+    public void drawAll(Graphics g){
+        for(Stick s : alSticks){
+        s.draw(g);
+    }
+        
+    }
+    
+    
     
 }
